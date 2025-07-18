9aef008b23fbc2f2ee3442efb913f0bd5c220437
RCD-2023 <codreanu_danielromeo@yahoo.com>
2025-07-18 09:07:48 +0300

    another commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,24 @@
+import { Card, CardHeader } from '@/components/ui/card';
+import { Metadata } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+};
+
 const SigInPage = () => {
-  return <>Sign In</>;
+  return (
+    <div className='w-full max-w-md mx-auto'>
+      <Card>
+        <CardHeader className='space-y-4'>
+          
+            <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME}`} />
+          </Link>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 };
 
 export default SigInPage;
